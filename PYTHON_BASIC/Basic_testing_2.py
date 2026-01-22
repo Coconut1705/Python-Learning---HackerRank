c2828d2e8ea12f533f173070324e9230ee371cc5
Nguyen Phu Dat <datdua2015@gmail.com>
2026-01-22 18:24:18 +0700

    tiep nua ne

---
+++ PYTHON_BASIC/Basic_testing_2.py
@@ -0,0 +1,22 @@
+a = 18
+b = 16
+if a > b:
+    print("Ngon luôn")
+else:
+    print("Chưa ngon lắm")
+
+
+conure_is_hungry = False
+if conure_is_hungry:
+    print("Dẹt đói")
+else:
+    print("Dẹt no rùi")
+
+def check(tuoi):
+    if tuoi >= 18:
+        print("Truy cập thành công!")
+    else:
+        print("Bị từ chối: Bạn chưa đủ tuổi.")
+
+check(20) # Kết quả: Truy cập thành công!
+check(17)
